d195957e3485387025e8fe795dc98d772684719a
JordanWilson <mrwizard6933@gmail.com>
2019-03-10 22:25:46 -0400

    made the 404 page

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { PageNotFoundComponent } from './page-not-found.component';
   ],
   imports: [
     PageNotFoundRoutingModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class PageNotFoundModule { }
